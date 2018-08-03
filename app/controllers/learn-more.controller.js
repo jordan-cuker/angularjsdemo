cabfb824061e4d40d5e59d1c9da599d6d79f2a84
jordan.cuker <jordan.cuker@accenture.com>
2018-08-03 14:24:41 -0500

    Fixed the html template.

--- app/controllers/learn-more.controller.js
+++ app/controllers/learn-more.controller.js
@@ -1,52 +1,55 @@
-angular.module("angularjsDemo")
-    .component("learnMoreController", {
-        controller: ($scope, learnmoreService) => {
-            $scope.shouldHideMore = function () {
-                return !learnmoreService.pageActive;
-            }
-            $scope.toggleMore = function(){
-                return learnmoreService.toggleMore(null);
-            }
-            $scope.getData = function(){
-                return learnmoreService.conceptData;
-            }
-            $scope.getTemplateUrl = function(){
-                console.log("../../views/" + conceptParam + ".html");
-                //return "../../views/" + $scope.conceptTitle + ".html";
-            }
-        },
-        bindings: {
-            conceptParam: '='
-        },
-        template: `<ng-include src="getTemplateUrl()"/>`
-    })
-
 // angular.module("angularjsDemo")
-//     .controller("learnmoreController", function($scope, learnmoreService){
-
-//         $scope.shouldHideMore = function (){
-//             return !learnmoreService.pageActive;
-//         }
-//         $scope.toggleMore = function(){
-//             return learnmoreService.toggleMore(null);
-//         }
-//         $scope.getData = function(){
-//             return learnmoreService.conceptData;
-//         }
+//     .component("learnMoreController", {
+//         controller: ($scope, learnmoreService) => {
+//             $scope.shouldHideMore = function () {
+//                 return !learnmoreService.pageActive;
+//             }
+//             $scope.toggleMore = function(){
+//                 return learnmoreService.toggleMore(null);
+//             }
+//             $scope.getData = function(){
+//                 return learnmoreService.conceptData;
+//             }
+//             this.$onInit = function(){
+//                 console.log("this init");
+//             }
+//             $scope.getTemplateUrl = function(){
+//                 //.console.log($scope.$ctrl);
+//                 //return "../../views/" + $scope.conceptTitle + ".html";
+//             }
+//         },
+//         bindings: {
+//             concept: '='
+//         },
+//         template: `<ng-include src="getTemplateUrl()"/>`
 //     })
-//     .directive("learnMore", function(){
-//         return{
-//             restrict: "E",
-//             template: `<ng-include src="getTemplateUrl()"/>`,
-//             scope: {
-//                 conceptTitle: '=conceptParam'
-//             },
-//             controller: function($scope) {
-//                 // function used on the ng-include to resolve the template
-//                 $scope.getTemplateUrl = function() {
-//                     console.log("../../views/" + $scope.conceptTitle + ".html");
-//                     return "../../views/" + $scope.conceptTitle + ".html";
-//                 }
-//               }
-//         }
-//     });
+
+angular.module("angularjsDemo")
+    .controller("learnmoreController", function($scope, learnmoreService){
+
+        $scope.shouldHideMore = function (){
+            return !learnmoreService.pageActive;
+        }
+        $scope.toggleMore = function(){
+            return learnmoreService.toggleMore(null);
+        }
+        $scope.getData = function(){
+            return learnmoreService.conceptData;
+        }
+    })
+    .directive("learnMoreController", function(){
+        return{
+            restrict: "E",
+            template: `<ng-include src="getTemplateUrl()"/>`,
+            scope: {
+                concept: '=concept'
+            },
+            controller: function($scope) {
+                // function used on the ng-include to resolve the template
+                $scope.getTemplateUrl = function() {
+                    //console.log($scope.concept);
+                    return "../../views/" + $scope.concept + ".html";
+                }
+              }
+        }
+    });
